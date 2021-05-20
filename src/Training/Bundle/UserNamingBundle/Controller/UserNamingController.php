a47495a6df7de5b756a7b554738eb141d3c2b3d4
Marcin Łukaszewicz <mlukaszewicz@oroinc.com>
2021-05-20 14:46:23 +0200

    Step 10: Dashboard widgets

--- src/Training/Bundle/UserNamingBundle/Controller/UserNamingController.php
+++ src/Training/Bundle/UserNamingBundle/Controller/UserNamingController.php
@@ -2,13 +2,15 @@
 
 namespace Training\Bundle\UserNamingBundle\Controller;
 
+use Oro\Bundle\DashboardBundle\Model\WidgetConfigs;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 use Oro\Bundle\SecurityBundle\Annotation\Acl;
 use Training\Bundle\UserNamingBundle\Entity\UserNamingType;
 
-class UserNamingController
+class UserNamingController extends AbstractController
 {
     /**
      * @Route("/", name="training_user_naming_index")
@@ -42,5 +44,18 @@ class UserNamingController
         return [
             'entity' => $type,
         ];
+    }
+
+    /**
+     * @Route("/dashboard/user-information", name="training_user_dashboard_user_information", options={"expose"=true})
+     * @Template
+     * @param WidgetConfigs $widgetConfigs
+     * @return array
+     */
+    public function dashboardUserInformationAction(WidgetConfigs $widgetConfigs): array
+    {
+        $data = $widgetConfigs->getWidgetAttributesForTwig('current_user_information');
+        $data['currentUser'] = $this->getUser();
+        return $data;
     }
 }
