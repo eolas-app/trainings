c5d4bf9f7b45b4aa8bd3174f5292951cab7f6906
Marcin Łukaszewicz <mlukaszewicz@oroinc.com>
2021-05-20 13:23:07 +0200

    Step 3: Security

--- src/Training/Bundle/UserNamingBundle/Controller/UserNamingController.php
+++ src/Training/Bundle/UserNamingBundle/Controller/UserNamingController.php
@@ -4,6 +4,8 @@ namespace Training\Bundle\UserNamingBundle\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+use Oro\Bundle\SecurityBundle\Annotation\Acl;
 use Training\Bundle\UserNamingBundle\Entity\UserNamingType;
 
 class UserNamingController
@@ -11,6 +13,7 @@ class UserNamingController
     /**
      * @Route("/", name="training_user_naming_index")
      * @Template
+     * @AclAncestor("training_user_naming_view")
      *
      * @return array
      */
@@ -24,6 +27,12 @@ class UserNamingController
     /**
      * @Route("/view/{id}", name="training_user_naming_view", requirements={"id"="\d+"})
      * @Template
+     * @Acl(
+     *      id="training_user_naming_view",
+     *      type="entity",
+     *      class="TrainingUserNamingBundle:UserNamingType",
+     *      permission="VIEW"
+     * )
      *
      * @param UserNamingType $type
      * @return array
